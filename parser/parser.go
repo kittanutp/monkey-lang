5a458fb92387fb068d7e1e84baa3a63009e281e8
kittanutp <kittanut.pong@gmail.com>
2024-04-08 17:56:59 +0700

    add parse program

--- parser/parser.go
+++ parser/parser.go
@@ -25,5 +25,54 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+	for !p.curTokenIs(token.EOF) {
+		stm := p.parseStament()
+		if stm != nil {
+			program.Statements = append(program.Statements, stm)
+		}
+		p.nextToken()
+
+	}
+	return program
+}
+
+func (p *Parser) parseStament() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStament()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStament() *ast.LetStatement {
+	stm := &ast.LetStatement{Token: p.curToken}
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+	stm.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stm
+}
+
+func (p *Parser) curTokenIs(tokenType token.TokenType) bool {
+	return p.curToken.Type == tokenType
+}
+
+func (p *Parser) expectPeek(tokenType token.TokenType) bool {
+	if p.peekTokenIs(tokenType) {
+		p.nextToken()
+		return true
+	}
+	return false
+}
+func (p *Parser) peekTokenIs(tokenType token.TokenType) bool {
+	return p.peekToken.Type == tokenType
 }
