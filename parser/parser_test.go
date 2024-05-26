81306d5240a636d5352129021ce25dea066e792d
kittanutp <kittanut.pong@gmail.com>
2024-05-26 21:29:46 +0700

    add precedence check

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -247,3 +247,52 @@ func TestParsingInfixExpression(t *testing.T) {
 
 	}
 }
+
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{"-a * b", "((-a) * b)"},
+		{"!-a", "(!(-a))"},
+		{"a + b + c", "((a + b) + c)"},
+		{"a + b - c", "((a + b) - c)"},
+		{"a * b * c", "((a * b) * c)"},
+		{"a * b / c", "((a * b) / c)"},
+		{"a + b / c", "(a + (b / c))"},
+		{"a + b * c + d / e - f", "(((a + (b * c)) + (d / e)) - f)"},
+		{"3 + 4; -5 * 5", "(3 + 4)((-5) * 5)"},
+		{"5 > 4 == 3 < 4", "((5 > 4) == (3 < 4))"},
+		{"5 < 4 != 3 > 4", "((5 < 4) != (3 > 4))"},
+		{"3 + 4 * 5 == 3 * 1 + 4 * 5", "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))"},
+		// {"true", "true"},
+		// {"false", "false"},
+		// {"3 > 5 == false", "((3 > 5) == false)"},
+		// {"3 < 5 == true", "((3 < 5) == true)"},
+		// {"1 + (2 + 3) + 4", "((1 + (2 + 3)) + 4)"},
+		// {"(5 + 5) * 2", "((5 + 5) * 2)"},
+		// {"2 / (5 + 5)", "(2 / (5 + 5))"},
+		// {"-(5 + 5)", "(-(5 + 5))"},
+		// {"!(true == true)", "(!(true == true))"},
+		// {"a + add(b * c) + d", "((a + add((b * c))) + d)"},
+		// {
+		// 	"add(a, b, 1, 2 * 3, 4 + 5, add(6, 7 * 8))",
+		// 	"add(a, b, 1, (2 * 3), (4 + 5), add(6, (7 * 8)))",
+		// },
+		// {"add(a + b + c * d / f + g)", "add((((a + b) + ((c * d) / f)) + g))"},
+		// {"a * [1, 2, 3, 4][b * c] * d", "((a * ([1, 2, 3, 4][(b * c)])) * d)"},
+		// {"add(a * b[2], b[1], 2 * [1, 2][1])", "add((a * (b[2])), (b[1]), (2 * ([1, 2][1])))"},
+	}
+
+	for _, tt := range tests {
+		p := New(lexer.New(tt.input))
+
+		program := p.ParseProgram()
+		checkParseErrros(t, p)
+
+		actual := program.String()
+		if actual != tt.expected {
+			t.Errorf("expected=%q, got=%q", tt.expected, actual)
+		}
+	}
+}
